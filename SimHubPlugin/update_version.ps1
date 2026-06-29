6a99e185e1ff17c598de283e328204c0f60094bb
tcfshcrw <48719709+tcfshcrw@users.noreply.github.com>
2026-06-29 20:07:04 +0800

    Update update_version.ps1

--- SimHubPlugin/update_version.ps1
+++ SimHubPlugin/update_version.ps1
@@ -1,12 +1,14 @@
 $date = Get-Date
 $year = $date.ToString("yy")
-$week = (Get-Date -UFormat "%V")
+# ISO 8601 week number calculation for Windows PowerShell compatibility
+$week = [System.Globalization.CultureInfo]::InvariantCulture.Calendar.GetWeekOfYear($date, [System.Globalization.CalendarWeekRule]::FirstFourDayWeek, [System.DayOfWeek]::Monday)
+$weekStr = $week.ToString("00")
 $day = [int]$date.DayOfWeek
 if ($day -eq 0) {
     $day = 7
 }
 $dayStr = $day.ToString("00")
-$version = "${year}.${week}.${dayStr}"
+$version = "${year}.${weekStr}.${dayStr}"
 
 $filepath = "VariablesStruct\constants.cs"
 if (Test-Path $filepath) {
