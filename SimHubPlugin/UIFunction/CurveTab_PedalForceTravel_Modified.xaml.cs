31ce014e8b8a244d1174fc1710a226b107ec6ec9
DESKTOP-T5N4KAS\blastodd <toddwizz@gmail.com>
2026-07-01 01:43:25 +0300

    chore: rename textbox variables

--- SimHubPlugin/UIFunction/CurveTab_PedalForceTravel_Modified.xaml.cs
+++ SimHubPlugin/UIFunction/CurveTab_PedalForceTravel_Modified.xaml.cs
@@ -549,28 +549,28 @@ namespace DiyFfbPedal.UIFunction
         private void Label_max_force_MouseDoubleClick(object sender, MouseButtonEventArgs e)
         {
             if (Rangeslider_force_range != null)
-                BeginInlineEdit(Label_max_force, Edit_max_force, Rangeslider_force_range.UpperValue);
+                BeginInlineEdit(Label_max_force, TextBox_edit_max_force, Rangeslider_force_range.UpperValue);
             e.Handled = true;
         }
 
         private void Label_min_force_MouseDoubleClick(object sender, MouseButtonEventArgs e)
         {
             if (Rangeslider_force_range != null)
-                BeginInlineEdit(Label_min_force, Edit_min_force, Rangeslider_force_range.LowerValue);
+                BeginInlineEdit(Label_min_force, TextBox_edit_min_force, Rangeslider_force_range.LowerValue);
             e.Handled = true;
         }
 
         private void Label_min_pos_MouseDoubleClick(object sender, MouseButtonEventArgs e)
         {
             if (Rangeslider_travel_range != null)
-                BeginInlineEdit(Label_min_pos, Edit_min_pos, CurrentTravelMm(Rangeslider_travel_range.LowerValue));
+                BeginInlineEdit(Label_min_pos, TextBox_edit_min_pos, CurrentTravelMm(Rangeslider_travel_range.LowerValue));
             e.Handled = true;
         }
 
         private void Label_max_pos_MouseDoubleClick(object sender, MouseButtonEventArgs e)
         {
             if (Rangeslider_travel_range != null)
-                BeginInlineEdit(Label_max_pos, Edit_max_pos, CurrentTravelMm(Rangeslider_travel_range.UpperValue));
+                BeginInlineEdit(Label_max_pos, TextBox_edit_max_pos, CurrentTravelMm(Rangeslider_travel_range.UpperValue));
             e.Handled = true;
         }
 
@@ -612,10 +612,10 @@ namespace DiyFfbPedal.UIFunction
             string text = (box.Text ?? string.Empty).Trim().Replace(',', '.');
             if (double.TryParse(text, System.Globalization.NumberStyles.Any, System.Globalization.CultureInfo.InvariantCulture, out double value))
             {
-                if (box == Edit_max_force) SetForceValue(true, value);
-                else if (box == Edit_min_force) SetForceValue(false, value);
-                else if (box == Edit_min_pos) SetTravelMm(false, value);
-                else if (box == Edit_max_pos) SetTravelMm(true, value);
+                if (box == TextBox_edit_max_force) SetForceValue(true, value);
+                else if (box == TextBox_edit_min_force) SetForceValue(false, value);
+                else if (box == TextBox_edit_min_pos) SetTravelMm(false, value);
+                else if (box == TextBox_edit_max_pos) SetTravelMm(true, value);
             }
             EndInlineEdit(box);
         }
@@ -624,10 +624,10 @@ namespace DiyFfbPedal.UIFunction
         {
             box.Visibility = Visibility.Collapsed;
             Label label = null;
-            if (box == Edit_max_force) label = Label_max_force;
-            else if (box == Edit_min_force) label = Label_min_force;
-            else if (box == Edit_min_pos) label = Label_min_pos;
-            else if (box == Edit_max_pos) label = Label_max_pos;
+            if (box == TextBox_edit_max_force) label = Label_max_force;
+            else if (box == TextBox_edit_min_force) label = Label_min_force;
+            else if (box == TextBox_edit_min_pos) label = Label_min_pos;
+            else if (box == TextBox_edit_max_pos) label = Label_max_pos;
             if (label != null) label.Visibility = Visibility.Visible;
         }
 
